6a2384dd65c989351f7dcb841e54c6c582387742
Adam Ever-Hadani <adamhadani@gmail.com>
2014-02-07 17:26:09 -0800

    iOS, updates to WKTParser to parse WKT point

--- WKTParser/WKTParser.m
+++ WKTParser/WKTParser.m
@@ -12,7 +12,19 @@
 
 @implementation WKTParser
 
-+(NSArray*)polygonsForWkt:(NSString*)wkt {
+#pragma mark - Public Parsing Methods
+
++(CLLocationCoordinate2D)locationForWktPoint:(NSString *)wkt
+{
+    NSAssert(wkt && [wkt rangeOfString: @"POINT"].location != NSNotFound, @"Must pass a WKT POINT String");
+    
+    CLLocation *coordinates = [self coordinatesForWktPoint:wkt];
+    CLLocationCoordinate2D location = CLLocationCoordinate2DMake(coordinates.coordinate.latitude, coordinates.coordinate.longitude);
+    
+    return location;
+}
+
++(NSArray *)polygonsForWkt:(NSString *)wkt {
     NSMutableArray *polygons = [NSMutableArray array];
     
     if ([wkt rangeOfString: @"MULTIPOLYGON"].location == NSNotFound)
@@ -35,16 +47,18 @@
     return [NSArray arrayWithArray: polygons];
 }
 
-+(NSArray*)splitMultiPolygonsFromWkt:(NSString*)wkt {
+#pragma mark - Private Methods
+
++(NSArray *)splitMultiPolygonsFromWkt:(NSString *)wkt {
     return [wkt componentsSeparatedByString: @")),(("];
 }
 
-+(MKPolygon*)polygonForWkt:(NSString*)wkt {
++(MKPolygon *)polygonForWkt:(NSString *)wkt {
     NSMutableArray *coordinates = [self coordinatesForWkt: wkt];
     return [self polygonForCoordinates: coordinates];
 }
 
-+(NSMutableArray*)coordinatesForWkt:(NSString*)wkt {
++(NSMutableArray *)coordinatesForWkt:(NSString *)wkt {
     
     NSMutableArray *coordinates = [NSMutableArray array];
     
@@ -70,7 +84,29 @@
     return coordinates;
 }
 
-+(MKPolygon*)polygonForCoordinates:(NSArray*) coordinates {
++(CLLocation *)coordinatesForWktPoint:(NSString *)wkt
+{
+    NSMutableArray *coordinates = [NSMutableArray array];
+    
+    NSError *error = NULL;
+
+    NSString *coordinatesRegexString = @"([-\\d\\.]+\\s[-\\d\\.]+)";
+    NSRegularExpression *coordinatesRegex = [NSRegularExpression regularExpressionWithPattern: coordinatesRegexString
+                                                                                      options: NSRegularExpressionCaseInsensitive
+                                                                                        error: &error];
+    NSArray *coordinatesStrings = [coordinatesRegex matchesInString: wkt
+                                                            options: 0
+                                                              range: NSMakeRange(0, [wkt length])];
+    
+    for (NSTextCheckingResult *match in coordinatesStrings) {
+        NSRange matchRange = [match rangeAtIndex:1];
+        [coordinates addObject:[self parseCoordinateFromString: [wkt substringWithRange:matchRange]]];
+    }
+    
+    return coordinates[0];
+}
+
++(MKPolygon *)polygonForCoordinates:(NSArray *) coordinates {
     
 	NSInteger coordsLen = [coordinates count] - 1;
     
@@ -90,7 +126,7 @@
 	return newPolygon;
 }
 
-+(CLLocation*)parseCoordinateFromString:(NSString*)coordinateString {
++(CLLocation *)parseCoordinateFromString:(NSString *)coordinateString {
     NSArray *points = [coordinateString componentsSeparatedByString:@" "];
     NSString *lon = [points objectAtIndex:0];
     NSString *lat = [points objectAtIndex:1];
